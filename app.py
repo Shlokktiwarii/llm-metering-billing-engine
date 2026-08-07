af7f91767256e2ba62bd85ac6c3210424bf8e279
Shlok Tiwari <shlokt16@gmail.com>
2026-08-07 23:51:04 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+from config import settings
+
+
+engine = create_engine(settings.DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = DeclarativeBase()
+
+print(settings.DATABASE_URL)
+app = FastAPI(
+    title="Usage Metering & Billing Engine",
+    version="1.0.0",
+)
+
+@app.get("/")
+def health_check():
+    return {
+        "status": "running",
+        "service": "Usage Metering & Billing Engine"
+    }
